ccb28c0ed43249825615bbd15fc1d6fc885c0ccb
Ioannis Loudaros <johnloudaros@hotmail.com>
2025-04-24 15:37:47 +0300

    Initials

---
+++ isetup.sh
@@ -0,0 +1,8 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Just and makefile
+brew install just
+brew install make
+
+
