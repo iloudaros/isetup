1807ecf15d9065512490ad6aa0b79c806423a03d
Ioannis Loudaros <johnloudaros@hotmail.com>
2025-04-25 21:05:38 +0300

    cheatsheets

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # isetup
  My quick repo for when I come to a new pc
+
+
+# First things first  
+
+
+## How to start
+1. Clone the repo to your local machine. You can do this by running the following command in your terminal:
+
+`git clone https://github.com/iloudaros/isetup.git`
+
+2. Navigate to the cloned directory:
+`cd isetup`
+
+3. Run the setup script:
+`bash isetup.sh`
+
+
+## What doe this repo contain?
+
+- `isetup.sh`: The main script that will install brew
+
+
+# Useful links
+- [Just Cheat Sheet](https://cheatography.com/linux-china/cheat-sheets/justfile/)
+- [Just VS Code](https://marketplace.visualstudio.com/items?itemName=skellock.just)
--- isetup.sh
+++ isetup.sh
@@ -1,8 +1,51 @@
+#!/bin/bash
+
+# Function to prompt user for confirmation
+confirm() {
+    read -p "$1 (y/n): " choice
+    case "$choice" in
+        y|Y ) return 0;;
+        n|N ) return 1;;
+        * ) echo "Invalid input. Please enter y or n."; confirm "$1";;
+    esac
+}
+
+# Ask user for mode of operation
+echo "Do you want to run everything automatically or confirm each step?"
+read -p "Enter 'a' for automatic or 'c' for confirm: " mode
+
+if [[ "$mode" != "a" && "$mode" != "c" ]]; then
+    echo "Invalid input. Please restart the script and enter 'a' or 'c'."
+    exit 1
+fi
+
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ "$mode" == "a" || ( "$mode" == "c" && confirm "Install Homebrew?" ) ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# Install Just and makefile
-brew install just
-brew install make
+# Install zsh and oh-my-zsh
+if [[ "$mode" == "a" || ( "$mode" == "c" && confirm "Install zsh and oh-my-zsh?" ) ]]; then
+    brew install zsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Install Just and make
+if [[ "$mode" == "a" || ( "$mode" == "c" && confirm "Install Just and make?" ) ]]; then
+    brew install just
+    brew install make
+fi
+
+# Install Git
+if [[ "$mode" == "a" || ( "$mode" == "c" && confirm "Install Rust?" ) ]]; then
+    brew install git
+fi
+
+# Install gh
+if [[ "$mode" == "a" || ( "$mode" == "c" && confirm "Install GitHub CLI (gh)?" ) ]]; then
+    brew install gh
+fi
 
 
+# Exit message
+echo "Setup completed successfully!"
